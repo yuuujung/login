7b195e486bd622c99622aa9d74d43b43dc4e19f1
yuuujung <dbwjd980611@naver.com>
2023-05-27 23:54:41 +0900

    230527_2

---
+++ .nuxt/router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _defb758e = () => interopDefault(import('../pages/landing.vue' /* webpackChunkName: "pages/landing" */))
+const _f587c61e = () => interopDefault(import('../pages/register.vue' /* webpackChunkName: "pages/register" */))
+const _74d0d8f4 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/landing",
+    component: _defb758e,
+    name: "landing"
+  }, {
+    path: "/register",
+    component: _f587c61e,
+    name: "register"
+  }, {
+    path: "/",
+    component: _74d0d8f4,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
